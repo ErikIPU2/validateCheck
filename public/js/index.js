eedf34b6a9c769ecbc99e67826b055455aa64471
erikborella <erik02_palmeiras@hotmail.com>
2018-06-02 17:21:25 -0300

    Build 1

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,6 @@
 $(document).ready(() => {
     $(".modal").modal();
+    $('.dropdown-trigger').dropdown();
     $('.datepicker').datepicker({
         format : 'dd/mm/yyyy'
     });
@@ -20,39 +21,42 @@ $("#date").change(() => {
     
 })
 
+$("#radioBar").change(() => {
+    $("#barcodeS").removeAttr('disabled');
+    $("#nameS").attr('disabled', 'disabled');
+})
+
+$("#radioName").change(() => {
+    $("#nameS").removeAttr('disabled');
+    $("#barcodeS").attr('disabled', 'disabled');
+})
+
 $("#addSend").click(() => {
-    var form = $("#addForm").serializeArray();
-    console.log(form);
-    
+    var form = $("#addForm").serializeArray();    
     
     if (!form[0].value) {
         iziToast.error({
-            title: "Digite o nome do produto",
-            position: "topRight"
+            title: "Digite o nome do produto"
         });
     }
     else if (!form[1].value) {
         iziToast.error({
-            title: "Digite o codigo de barras",
-            position: "topRight"
+            title: "Digite o codigo de barras"
         });
     }
     else if (!form[2].value) {
         iziToast.error({
-            title: "Digite o preço",
-            position: "topRight"
+            title: "Digite o preço"
         });
     }
     else if (!form[3].value) {
         iziToast.error({
-            title: "Digite a data de validade",
-            position: "topRight"
+            title: "Digite a data de validade"
         });
     }
     else if (!isValid($("#date").val())) {
         iziToast.error({
-            title: "Produto já vencido",
-            position: "topRight"
+            title: "Produto já vencido"
         });
     }
     else {
@@ -65,7 +69,17 @@ $("#addSend").click(() => {
             }, 
             
             (data, status) => {
-                console.log(data);
+                if (data) {
+                    iziToast.success({
+                        title: "Produto adionado",
+                        position: "topRight"
+                    });
+                    $("#name").val("");
+                    $("#barcode").val("");
+                    $("#preco").val("");
+                    $("#date").val("");
+                    M.updateTextFields();
+                }
             }
         )
     }
@@ -73,7 +87,178 @@ $("#addSend").click(() => {
     
 });
 
+$("#findAll").click(() => {
+    $.get("/getProd", (data, status) => {
+        $("#tableView").html(GENERATORTABLE(data));
+    })
+})
 
+$("#findInvalid").click(() => {
+    $.get("/getProd", (data, status) => {
+        var prop = [];
+        for (var i = 0; i < data.length; i++) {
+            if (!isValid(data[i].valid)) {
+                prop.push(data[i]);
+            }
+        }
+        $("#tableView").html(GENERATORTABLE(prop));
+        
+    });
+})
+
+$("#findAllM").click(() => {
+    $.get("/getProd", (data, status) => {
+        $("#tableView").html(GENERATORTABLE(data));
+    })
+})
+
+$("#findInvalidM").click(() => {
+    $.get("/getProd", (data, status) => {
+        var prop = [];
+        for (var i = 0; i < data.length; i++) {
+            if (!isValid(data[i].valid)) {
+                prop.push(data[i]);
+            }
+        }
+        $("#tableView").html(GENERATORTABLE(prop));
+        
+    });
+})
+
+$("#prodS").click(() => {
+    var form = ($("#searchForm").serializeArray())[0].value;
+    if (form == "barcode") {
+        var barcode = $("#barcodeS").val();
+        if (!barcode) {
+            iziToast.error({
+                title: "Digite o codigo de barras"
+            })
+        }
+        else {
+            $.get("/getProd", (data, status) => {
+                var prop = [];
+                for (var i = 0; i < data.length; i++) {
+                    if (data[i].barcode == barcode) {
+                        prop.push(data[i]);
+                    }
+                }
+
+                if (!prop.length) {
+                    iziToast.warning({
+                        title: "Nenhum produto encontrado"
+                    })
+                }
+                else {
+                    $("#tableView").html(GENERATORTABLE(prop));
+
+                    iziToast.info({
+                        title: prop.length + " produtos encontrados",
+                        position: "bottomLeft"
+                    });
+
+                    $("#barcodeS").val("");
+                    $("#nameS").val("");
+                    M.updateTextFields();
+
+                    $("#findModal").modal("close");
+                }
+            });
+        }
+        
+    }
+    else if (form == "name") {
+        var name = $("#nameS").val();
+        if (!name) {
+            iziToast.error({
+                title: "Digite o nome do produto"
+            })
+        }
+        else {
+            $.get("/getProd", (data, status) => {
+                var prop = [];
+                for (var i = 0; i < data.length; i++) {
+                    if (data[i].name == name) {
+                        prop.push(data[i]);
+                    }
+                }
+
+                if (!prop.length) {
+                    iziToast.warning({
+                        title: "Nenhum produto encontrado"
+                    })
+                }
+                else {
+                    $("#tableView").html(GENERATORTABLE(prop));
+                    iziToast.info({
+                        title: prop.length + " produtos encontrados",
+                        position: "bottomLeft"
+                    });
+                    $("#barcodeS").val("");
+                    $("#nameS").val("");
+                    M.updateTextFields();
+
+                    $("#findModal").modal("close");
+                }
+            })
+        }
+    }
+    
+    
+});
+
+function GENERATORTABLE(data) {
+
+    var cont = 0;
+    var prop = `<table class="striped ">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Codigo de barras</th>
+                            <th>Preço</th>
+                            <th>Data de validade</th>
+                        </tr>
+                    </thead>
+                    <tbody>`;
+    console.log(data);
+
+    for (var i = 0; i < data.length; i++) {
+        if (!isValid(data[i].valid)) {
+            cont++;
+            prop += `<tr class="red">
+                <td>`+data[i].name+`</td>
+                <td>`+data[i].barcode+`</td>
+                <td>`+data[i].price+` R$</td>
+                <td>`+data[i].valid+`</td>
+            </tr>`
+        }
+        else {
+            prop += `<tr>
+                        <td>`+data[i].name+`</td>
+                        <td>`+data[i].barcode+`</td>
+                        <td>`+data[i].price+` R$</td>
+                        <td>`+data[i].valid+`</td>
+                    </tr>`
+        }
+    }
+
+    prop += `</tbody>
+            </table>`;
+
+    if (cont == 0) {
+        iziToast.success({
+            title:"Nenhum produto vencido",
+        })
+    }
+    else {
+        iziToast.warning({
+            title: cont+" Produtos vencidos"
+
+        })
+    }
+
+    return prop;
+    
+}
 
 function getDay() {
 
