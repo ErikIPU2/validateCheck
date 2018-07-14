0fe94a94f477c5bbfe039b60f22e7ef12d517347
ErikIPU2 <erikborella@gmail.com>
2018-07-14 18:30:09 -0300

    Build 1.3

--- public/js/index.js
+++ public/js/index.js
@@ -301,8 +301,19 @@ $("#prodS").click(() => {
     
 });
 
+$("#clearBtn").click(() => {
+    $("#name").val("");
+    $("#barcode").val("");
+    $("#preco").val("");
+    $("#date").val("");
+    M.updateTextFields();
+})
+
 
 function GENERATORTABLE(data) {
+
+    data.sort(dynamicSort("name"));
+    
 
     var cont = 0;
     var prop = `<table class="striped ">
@@ -450,4 +461,16 @@ function existBarcode(barcode, data) {
         }
     }
     return bol; 
+}
+
+function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
 }
